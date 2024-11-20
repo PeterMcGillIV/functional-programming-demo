2fe0009613f1d9e9542cedd9b98c10a206a9f3c4
PeterMcGillIV <petermcgilliv@me.com>
2024-11-19 21:18:33 -0500

    Ready to submit

--- src/main.hs
+++ src/main.hs
@@ -1,11 +1,34 @@
 -- Write printAMessage here
+printAMessage :: Show a => a -> IO ()
+printAMessage x = putStrLn (show x)
 
 -- Write division here
+division :: Double -> Double -> Maybe Double
+division x y
+    | x == y    = Nothing
+    | otherwise = Just (x / y)
 
 -- Write factorial here
+factorial :: Int -> Int
+factorial 0 = 1
+factorial 1 = 1
+factorial n = n * factorial (n - 1)
 
 -- Write factList here
+factList :: Int -> [Int]
+factList n = [factorial i | i <- [1..n]]
 
 -- Write merge here
+merge :: [Int] -> [Int] -> [Int]
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys)
+    | x <= y    = x : merge xs (y:ys)
+    | otherwise = y : merge (x:xs) ys
 
-main = print "Test" -- Replace this with your testing code
+main = do -- Replace this with your testing code
+    printAMessage "Hello, Haskell!"
+    print (division 10 5)
+    print (factorial 5)
+    print (factList 5)
+    print (merge [1, 4, 5, 7] [2, 3, 6])
--- src/test.hs
+++ src/test.hs
@@ -1,2 +1,21 @@
 -- Feel free to experiment with Haskell in this file!
 -- This will not affect your assignment's grade
+addNums :: Int -> Int
+addNums 0 = 0
+addNums n = n + addNums (n - 1)
+
+-- Adds together the values taken to the given power of each number in the list.
+sumPowers :: Int -> [Int] -> Int
+sumPowers _ [] = 0
+sumPowers power (x : xs) = (x ^ power) + sumPowers power xs
+sumPowers2 = sumPowers 2
+sumPowers3 = sumPowers 3
+
+sum1 = sumPowers2 [1, 2, 3, 4] -- 30
+sum2 = sumPowers3 [1, 2, 3, 4] -- 100
+main = print (sum1)
+
+-- Calls a function on a Maybe value if it exists, returning the result, or returns a default value otherwise
+callOrDefault :: (a -> b) -> b -> Maybe a -> b
+callOrDefault fn _ (Just x) = fn x
+callOrDefault _ defaultValue Nothing = defaultValue
